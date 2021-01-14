8cdc25d2f171ec59c02d4b980134b0d450260351
Franz Holzinger <holzinger@ttproducts.de>
2021-01-14 18:53:57 +0100

    * compatibility TYPO3 10.4 and PHP 7.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.5.0-7.3.99',
-			'typo3' => '7.6.0-9.5.99',
+			'typo3' => '7.6.0-10.4.99',
 		),
 		'conflicts' => array(
 		),
