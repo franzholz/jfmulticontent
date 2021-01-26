01c6eb781b6dde6f55a8db48dd580943bed25b76
Franz Holzinger <holzinger@ttproducts.de>
2021-01-26 17:38:46 +0100

    -

--- ext_tables.php
+++ ext_tables.php
@@ -1,9 +1,12 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-if (
-    TYPO3_MODE == 'BE'
-) {
-    $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['JambageCom\\Jfmulticontent\\Controller\\Plugin\\WizardIcon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath(JFMULTICONTENT_EXT) . 'Classes/Controller/Plugin/WizardIcon.php';
-}
+call_user_func(function () {
 
+    if (
+        TYPO3_MODE == 'BE'
+    ) {
+        $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['JambageCom\\Jfmulticontent\\Controller\\Plugin\\WizardIcon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath(JFMULTICONTENT_EXT) . 'Classes/Controller/Plugin/WizardIcon.php';
+    }
+
+});
