9745f252550e4360b7bc5907a62a739418c18f29
Franz Holzinger <holzinger@ttproducts.de>
2021-02-04 08:57:07 +0100

    remove more deprecated configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,28 +4,26 @@
 # Extension Manager/Repository config file for ext "jfmulticontent".
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Multiple Content',
-	'description' => 'Arranges multiple contents into one content element with multiple columns, accordions, tabs, slider, slidedeck, easyAccordion or Booklet. This extension will also extend tt_news with two new lists.',
-	'category' => 'plugin',
-	'version' => '2.11.3',
-	'state' => 'stable',
-	'clearcacheonload' => 1,
-	'author' => 'Franz Holzinger, Juergen Furrer',
-	'author_email' => 'franz@ttproducts.de',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.5.0-7.3.99',
-			'typo3' => '7.6.0-10.4.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Multiple Content',
+    'description' => 'Arranges multiple contents into one content element with multiple columns, accordions, tabs, slider, slidedeck, easyAccordion or Booklet. This extension will also extend tt_news with two new lists.',
+    'category' => 'plugin',
+    'version' => '2.11.3',
+    'state' => 'stable',
+    'clearcacheonload' => 1,
+    'author' => 'Franz Holzinger, Jürgen Furrer',
+    'author_email' => 'franz@ttproducts.de',
+    'author_company' => '',
+    'constraints' => [
+        'depends' => [
+            'php' => '5.5.0-7.3.99',
+            'typo3' => '7.6.0-10.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
             'lib_jquery' => '2.1.0-0.0.0',
-			'patchlayout' => '0.0.1-0.1.9',
-		),
-	),
-);
+            'patchlayout' => '0.0.1-0.1.9',
+        ],
+    ],
+];
