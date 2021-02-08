1812443935c94f548a5210f264b1a639c36776f6
Franz Holzinger <holzinger@ttproducts.de>
2021-02-08 15:25:38 +0100

    php 7.4 must be possible

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.3.99',
+            'php' => '5.5.0-7.4.99',
             'typo3' => '7.6.0-10.4.99',
         ],
         'conflicts' => [
