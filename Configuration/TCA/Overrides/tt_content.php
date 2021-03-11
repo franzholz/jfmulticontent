09c2641dfb2d222380a591b3f48ccb915e3e0944
Franz Holzinger <holzinger@ttproducts.de>
2021-03-11 15:33:49 +0100

    * bugfix: The TCA field tx_jfmulticontent_contents must be of type select, if useStoragePidOnly == 1. In this case it must not be possible to select other tt_content records.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,12 +13,11 @@ call_user_func(function () {
     $GLOBALS['TCA'][$table]['ctrl']['requestUpdate'] .= ($GLOBALS['TCA'][$table]['ctrl']['requestUpdate'] ? ',' : '') . 'tx_jfmulticontent_view';
 
     $confArr = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][JFMULTICONTENT_EXT];
-
     $colPosOfIrreContent = intval($confArr['colPosOfIrreContent']);
 
-    if (!isset($GLOBALS['TCA']['tt_content']['columns']['colPos']['config']['items'][$colPosOfIrreContent])) {
+    if (!isset($GLOBALS['TCA'][$table]['columns']['colPos']['config']['items'][$colPosOfIrreContent])) {
         // Add the new colPos to the array, only if the ID does not exist...
-        $GLOBALS['TCA']['tt_content']['columns']['colPos']['config']['items'][$colPosOfIrreContent] = [
+        $GLOBALS['TCA'][$table]['columns']['colPos']['config']['items'][$colPosOfIrreContent] = [
                 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.colPosOfIrreContent',
                 $colPosOfIrreContent
         ];
@@ -90,40 +89,87 @@ call_user_func(function () {
         ],
     ];
 
-    $temporaryColumns['tx_jfmulticontent_contents'] = [
-        'exclude' => 1,
-        'displayCond' => 'FIELD:tx_jfmulticontent_view:IN:content',
-        'label' => 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.tx_jfmulticontent.contents',
-        'config' => [
-            'type' => 'group',
-            'internal_type' => 'db',
-            'allowed' => 'tt_content',
-            'size' => 12,
-            'minitems' => 0,
-            'maxitems' => 1000,
-            'fieldControl' => [
-                'elementBrowser' => [
-                    'disabled' => $confArr['useStoragePidOnly'],
-                ],
-                'addRecord' => [
-                    'disabled' => $confArr['useStoragePidOnly'],
-                    'pid' => '###PAGE_TSCONFIG_ID###',
-                    'options' => [
-                        'title'  => 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.tx_jfmulticontent.contents_add'
-                    ]
-                ],
-                'editPopup' => [
-                    'disabled' => false,
-                    'options' => [
-                        'title'  => 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.tx_jfmulticontent.contents_edit'
-                    ]
-                ],
-                'listModule' => [
-                    'disabled' => false
-                ],
+    if ($confArr['useStoragePidOnly']) {
+
+        $foreignTableWhere = '';
+        if (
+            version_compare(TYPO3_version, '9.0.0', '>=')
+        ) {
+            $foreignTableWhere = 'AND {#tt_content}.{#pid} = ###PAGE_TSCONFIG_ID### AND {#tt_content}.{#hidden} = 0 AND {#tt_content}.{#deleted} = 0 AND {#tt_content}.{#sys_language_uid} IN (0,-1) ORDER BY tt_content.uid';
+        } else {
+            $foreignTableWhere = 'AND tt_content.pid=###PAGE_TSCONFIG_ID### AND tt_content.hidden=0 AND tt_content.deleted=0 AND tt_content.sys_language_uid IN (0,-1) ORDER BY tt_content.uid';
+        }
+
+        $temporaryColumns['tx_jfmulticontent_contents'] = [
+            'exclude' => 1,
+            'displayCond' => 'FIELD:tx_jfmulticontent_view:IN:content',
+            'label' => 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.tx_jfmulticontent.contents',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectMultipleSideBySide',
+                'foreign_table' => 'tt_content',
+                'foreign_table_where' => $foreignTableWhere,
+                'size' => 6,
+                'autoSizeMax' => 20,
+                'minitems' => 0,
+                'maxitems' => 1000,
+                'fieldControl' => [ 
+                    'editPopup' => [ 
+                        'disabled' => false,
+                        'options' => [
+                            'title'  => 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.tx_jfmulticontent.contents_edit'
+                        ]
+                    ],
+                    'addRecord' => [ 
+                        'disabled' => true,
+                    ],
+                    'listModule' => [ 
+                        'disabled' => false,
+                    ],
+                ]
             ]
-        ]
-    ];
+        ];
+    } else {
+        $temporaryColumns['tx_jfmulticontent_contents'] = [
+            'exclude' => 1,
+            'displayCond' => 'FIELD:tx_jfmulticontent_view:IN:content',
+            'label' => 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.tx_jfmulticontent.contents',
+            'config' => [
+                'type' => 'group',
+                'internal_type' => 'db',
+                'allowed' => 'tt_content',
+                'size' => 12,
+                'minitems' => 0,
+                'maxitems' => 1000,
+                'suggestOptions' => [ 
+                'default' => [ 
+                    'pidList' => '###PAGE_TSCONFIG_ID###',
+                ],
+                ],
+                'fieldControl' => [
+                    'elementBrowser' => [
+                        'disabled' => $confArr['useStoragePidOnly'] == '1',
+                    ],
+                    'addRecord' => [
+                        'disabled' => $confArr['useStoragePidOnly'] == '1',
+                        'pid' => '###PAGE_TSCONFIG_ID###',
+                        'options' => [
+                            'title'  => 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.tx_jfmulticontent.contents_add'
+                        ]
+                    ],
+                    'editPopup' => [
+                        'disabled' => false,
+                        'options' => [
+                            'title'  => 'LLL:EXT:' . JFMULTICONTENT_EXT . '/locallang_db.xml:tt_content.tx_jfmulticontent.contents_edit'
+                        ]
+                    ],
+                    'listModule' => [
+                        'disabled' => false
+                    ],
+                ]
+            ]
+        ];
+    }
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $temporaryColumns);
 
