58cb453106b0653bb7e78b88157967fed7a47d29
Franz Holzinger <franz.holzinger@festwein.de>
2021-10-23 16:18:49 +0200

    PHP 7.2 is the minimum

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.4.99',
+            'php' => '7.2.0-7.4.99',
             'typo3' => '7.6.0-10.4.99'
         ],
         'conflicts' => [
