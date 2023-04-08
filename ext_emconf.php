e127cdc8364794682bc9f2e3a71477eaed85745e
Franz Holzinger <franz.holzinger@festwein.de>
2023-04-08 16:50:01 +0200

    add support for TYPO3 12. Remove TYPO3 9.5 and 10.4 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.4.0-8.1.99',
-            'typo3' => '9.5.0-11.5.99'
+            'typo3' => '11.5.0-12.3.99'
         ],
         'conflicts' => [
         ],
