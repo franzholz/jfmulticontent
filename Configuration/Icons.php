0ee7c91c66377c899c049b06cfc7266bd805fc45
Franz Holzinger <franz.holzinger@festwein.de>
2023-04-12 20:40:17 +0200

    content wizard by NewContentElement.tsconfig

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+   return [
+       'extensions-jfmulticontent-wizard' => [
+           'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+           // The source bitmap file
+           'source' => 'EXT:jfmulticontent/Resources/Public/Icons/ce_wiz.gif'
+       ],
+   ];
