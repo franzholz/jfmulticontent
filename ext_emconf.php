580a127c4f2523e4cf8d9249ecee7eafb9fcc7ef
Franz Holzinger <franz.holzinger@festwein.de>
2024-12-10 00:06:08 +0100

    compatibility 13, fix #21

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Multiple Content',
     'description' => 'It arranges multiple contents into one content element with multiple columns, accordions, tabs, slider, slidedeck, easyAccordion or Booklet. This extension will also extend tt_news with two new lists.',
     'category' => 'plugin',
-    'version' => '2.15.4',
+    'version' => '2.15.5',
     'state' => 'stable',
     'author' => 'Franz Holzinger, Jürgen Furrer',
     'author_email' => 'franz@ttproducts.de',
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'php' => '7.4.0-8.4.99',
             'typo3' => '12.4.0-13.4.99',
-            'div2007' => '2.0.6-0.0.0'
+            'div2007' => '2.2.0-0.0.0'
         ],
         'conflicts' => [
         ],
