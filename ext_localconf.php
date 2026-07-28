b5b815ab3dbc0a37530c8a1057312561a8e7ed9d
Franz Holzinger <franz.holzinger@festwein.de>
2026-07-28 22:03:05 +0200

    use the TYPO3 FileWriter

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,11 @@
 
 defined('TYPO3') || die('Access denied.');
 
+use Psr\Log\LogLevel;
+
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Log\Writer\FileWriter;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -12,16 +16,22 @@ call_user_func(function ($extensionKey): void {
     )->get($extensionKey);
 
     // Save the content
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$extensionKey] = \JambageCom\Jfmulticontent\Hooks\DataHandler::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$extensionKey] =
+        \JambageCom\Jfmulticontent\Hooks\DataHandler::class;
 
-    $GLOBALS['TYPO3_CONF_VARS']['LOG']['JambageCom']['Jfmulticontent'] = [
-        'writerConfiguration' => [
-            \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
-                \JambageCom\Jfmulticontent\Log\Writer\FileWriter::class => [
-                    'mode' => $extensionConfiguration['FILEWRITER']
+    if (!empty($extensionConfiguration['FILEWRITER'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['LOG']['JambageCom']['Jfmulticontent'] = [
+            'writerConfiguration' => [
+                // 1. Verwenden des PSR-3 String-Schlüssels ('debug')
+                LogLevel::DEBUG => [
+                    // 2. Nutzen des nativen Core-FileWriters
+                    FileWriter::class => [
+                        // Der Pfad zu Log-Dateien wird über Environment ausgelesen:
+                        'logFile' => Environment::getVarPath() . '/log/jfmulticontent.log'
+                    ]
                 ]
-            ]
-        ],
-    ];
+            ],
+        ];
+    }
 }, 'jfmulticontent');
 
